7c2a51dde72f59c20b28cc3b2d4a4bef2b903d76
BhavyashreeKP <bhavyashreekp11@gmail.com>
2025-04-11 19:38:54 +0530

    connecting_db

--- server/server.js
+++ server/server.js
@@ -1,15 +1,2 @@
-const mongoose = require("mongoose");
-const dotenv = require("dotenv");
-dotenv.config();
-
-mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => {
-    console.log("MongoDB connected");
-  })
-  .catch((err) => {
-    console.error("MongoDB connection error:", err);
-  });
+const connectDB = require('./config/db');
+connectDB(); // call the DB connection

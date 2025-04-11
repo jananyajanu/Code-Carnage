0d09df1cdda6754ed45a1f81d954aace3ce29e6d
KEERTHANA-M-2005 <keerthigowli05@gmail.com>
2025-04-12 00:10:58 +0530

    Land_Signup_Login_changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import HomePage from "./pages/HomePage";
+import LandingPage from "./pages/LandingPage";
+import RoleSelection from "./pages/RoleSelection";
 import Signup from "./pages/Signup";
-import UploadVideo from "./pages/UploadVideo";
-import VideoFeed from "./pages/VideoFeed";
-import Leaderboard from "./pages/Leaderboard";
-import AdminPanel from "./pages/AdminPanel";
 import Login from "./pages/Login";
+import HomePage from "./pages/HomePage"; // Your main app page
 
 function App() {
   return (
     <>
-      <Navbar />
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/select-role" element={<RoleSelection />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/upload" element={<UploadVideo />} />
-        <Route path="/feed" element={<VideoFeed />} />
-        <Route path="/leaderboard" element={<Leaderboard />} />
-        <Route path="/admin" element={<AdminPanel />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </>
   );

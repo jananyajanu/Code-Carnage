165338cc2d37011105d9855e6f367a6c9af25377
Jananya K H <jananyajanu33@gmail.com>
2025-05-06 17:37:05 +0530

    Name eroor

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,7 +9,7 @@ const generateToken = (id) => {
 // @desc    Register new user
 // @route   POST /api/users/register
 const registerUser = async (req, res) => {
-  const { username, email, password, role } = req.body;
+  const { name, email, password, role } = req.body;
 
   try {
     const userExists = await User.findOne({ email });
@@ -17,7 +17,7 @@ const registerUser = async (req, res) => {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const user = await User.create({ username, email, password, role });
+    const user = await User.create({ name, email, password, role });
 
     res.status(201).json({
       _id: user._id,
